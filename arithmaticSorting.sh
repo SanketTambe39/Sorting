357740294d0e277008793d7ccab5eae2acd73c85
SanketTambe39 <sankettambe39@gmail.com>
2020-08-04 13:43:33 +0530

    All oprations done

--- arithmaticSorting.sh
+++ arithmaticSorting.sh
@@ -18,3 +18,8 @@ echo "Answer of Second opration is "$opration2
 opration3=$(($num3+$num1/$num2))
 
 echo "Answer of third opration is "$opration3
+
+opration4=$(($num1%$num2+$num3))
+
+echo "Answer Of fourth opration is "$opration4
+
