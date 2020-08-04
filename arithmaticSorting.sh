27902851e85424ea4dc4835e9e59a0ca2be65af0
SanketTambe39 <sankettambe39@gmail.com>
2020-08-04 14:22:33 +0530

    All Data Read from Dictionary into array

--- arithmaticSorting.sh
+++ arithmaticSorting.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+#!/bin/bash
 
 declare -A compDict
 
@@ -10,23 +10,31 @@ echo "Your input is :: "$num1" "$num2" "$num3
 
 opration1=$(($num1+$num2*$num3))
 
-echo "Answer of First opration is "$opration1
+echo "Answer of First opration is :: "$opration1
 
 opration2=$(($num1*$num2+$num3))
 
-echo "Answer of Second opration is "$opration2
+echo "Answer of Second opration is :: "$opration2
 
 opration3=$(($num3+$num1/$num2))
 
-echo "Answer of third opration is "$opration3
+echo "Answer of third opration is :: "$opration3
 
 opration4=$(($num1%$num2+$num3))
 
-echo "Answer Of fourth opration is "$opration4
+echo "Answer Of fourth opration is :: "$opration4
 
 compDict[op1]=$opration1
 compDict[op2]=$opration2
 compDict[op3]=$opration3
 compDict[op4]=$opration4
 
-echo ${compDict[@]}
+echo "All data from Dictionary :: "${compDict[@]}
+
+count=0
+for i in ${compDict[@]}
+do
+	compArr[count++]=$i
+done
+
+echo "Data from array :: "${compArr[@]}
