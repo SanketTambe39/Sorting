ce061b073806a06050ffb0db31ec448464f09a35
SanketTambe39 <sankettambe39@gmail.com>
2020-08-04 13:13:26 +0530

    Accepting_elements

---
+++ arithmaticSorting.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+read -p " Enter number first number " num1
+read -p " Enter number first number " num2
+read -p " Enter number first number " num3
+
+echo "Your input is :: "$num1" "$num2" "$num3
+
